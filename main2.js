7c23756222a65adc2ce0b43f72521e97b411bfa6
camilohm1 <cahernandez3510@misena.edu.co>
2022-11-01 11:59:30 -0500

    trabajo-js

---
+++ main2.js
@@ -0,0 +1,40 @@
+addEventListener("DOMContentLoaded", (e)=>{
+let hor2=document.querySelector('#elint')
+let cont=1
+hor2.addEventListener("submit",(e)=>{
+    e.proventDefault();
+    if (cont<=50){
+    let datosEntrada = Object.fromEntries(new FormData(e.target))
+        let hor = datosEntrada.nhoras
+        let valh =5208.33
+        let pgh= valh*hor
+
+        let tabla=document.querySelector("tbody");
+        tabla.insertAdjacentHTML("beforeend",`
+        <tr>
+            <td>${cont}</td>
+            <td>${hor}</td>
+            <td>${valh}</td>
+            <td>${pgh}</td>
+        </tr>
+            `)
+            cont++
+        }
+    })
+})
+
+const c = document.querySelector(".container");
+const b = document.querySelector("button");
+
+function change() {
+  const
+    { width: cWidth, height: cHeight } = c.getBoundingClientRect(),
+    { width: bWidth, height: bHeight } = b.getBoundingClientRect(),
+    i = Math.floor(Math.random() * (cWidth - bWidth)) + 1,
+    j = Math.floor(Math.random() * (cHeight - bHeight)) + 1;
+
+  b.style.left = i + "px";
+  b.style.top = j + "px";
+}
+
+b.addEventListener("click", change);
